062e641779d9322f8248593994d0ceed97e95df3
Vitaliy Filipov <altras@gmail.com>
2014-11-08 01:12:47 +0200

    remove bootstrap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,26 +1,127 @@
 /** @jsx React.DOM */
-// require("./bootstrap/bootstrap.min.js");
-$ = require("jquery");
+window.$ = require("jquery");
+// require("./vendor/bootstrap/bootstrap.min.js");
 
 var React = window.React = require('react');
 var mountNode = document.getElementById("app");
 
+var SetIntervalMixin = {
+    componentWillMount: function() {
+        this.intervals = [];
+    },
+    setInterval: function(fn, ms) {
+        this.intervals.push(setInterval(fn, ms));
+    },
+    componentWillUnmount: function() {
+        this.intervals.forEach(clearInterval);
+    }
+};
+
 var KukerLeaderboard = React.createClass({
+  mixins: [SetIntervalMixin],
   componentWillMount: function() {
-    $.get("/json/mock.json").success(function(data){
-      var teams;
-      console.log(data);
-      debugger;
+    var self = this;
+    $.ajax({
+      dataType: "json",
+      url: "/json/mock.json",
+      success: function(data) {
+        console.log(data);
+        self.setState(data);
+      },
+      error: function(error) {
+        debugger;
+        console.error(error);     
+      }  
     });
-    return null;
   },
-  onChange: function(e) {
-    // this.setState({text: e.target.value});
+  componentDidMount: function() {
+    var self = this;
+
+    this.setInterval(function() {
+      $.ajax({
+        dataType: "json",
+        url: "/json/mock.json",
+        success: function(data) {
+          console.log(data);
+          self.setState(data);
+        },
+        error: function(error) {
+          debugger;
+          console.error(error);     
+        }  
+      })
+    }, 3000);
   },
   render: function() {
+    var elements = !this.state ? null : 
+        <div className="table-container">
+          <h1> Kuker Leaderboard </h1>
+          <table className="table table-striped table-hover ">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Team Name</th>
+                <th>Points</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><img className="podium" src="/images/podium.png"/></td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr className="info">
+                <td>3</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr className="success">
+                <td>4</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr className="danger">
+                <td>5</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr className="warning">
+                <td>6</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr className="active">
+                <td>7</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr className="active">
+                <td>8</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr className="active">
+                <td>9</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+              <tr className="active">
+                <td>10</td>
+                <td>{this.state.teams[0].teamName}</td>
+                <td>{this.state.teams[0].points}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    console.log(this.state);
     return (
       <div>
-        <h1> Kuker Leaderboard </h1>
+       {elements}
       </div>
     );
   }
